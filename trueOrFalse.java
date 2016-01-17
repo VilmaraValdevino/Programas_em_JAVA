d7a010e70553873353c81abea53ae6ca4acaf8a6
Vilmara de Oliveira Valdevino <vilmaravaldevino@globomail.com>
2016-01-17 20:03:21 -0200

    Create trueOrFalse.java
    Programinha simples com o funcionamento da variável lógica de verdadeiro ou falso, observamos que o valor da variável pode mudar caso mudemos os valores atribuídos à ela, se no caso por exemplo ao invés de atribuir "1==1" , atribuirmos "1==2" o valor que a variável "a" iria possuir  seria "false".

---
+++ trueOrFalse.java
@@ -0,0 +1,14 @@
+package script;
+
+public class verdadeiroOuFalso 
+{
+
+	public static void main(String[] args) 
+	{
+		//declaramdo variável do tipo lógica verdadeiro ou falso
+		boolean a = 1==1;
+		
+		//imprimindo na tela o valor que "A" possui, "true" ou "false"...
+		System.out.println( a );
+	}
+}
